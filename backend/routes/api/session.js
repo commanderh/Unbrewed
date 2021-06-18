27b2a55d680b9cafac052baf172997d7dafa2ae6
Huy Lam <commanderh1995@gmail.com>
2021-06-18 11:28:27 -0500

    finished getting user session,login, signup

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -25,4 +25,20 @@ router.post('/', asyncHandler(async(req, res, next) => {
 	return res.json({ user });
 }));
 
+
+//LOGGING OUT
+router.delete('/', (_req, res) => {
+    res.clearCookie('token');
+    return res.json({ message: 'success' });
+  }
+);
+
+router.get('/', restoreUser, (req, res) => {
+    const { user } = req;
+    if (user) {
+      return res.json({ user: user.toSafeObject() });
+    } else return res.json({});
+  }
+);
+
 module.exports = router;
