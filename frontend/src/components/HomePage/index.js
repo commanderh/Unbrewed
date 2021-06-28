f18075c994f973cde77bdffa8fa8e3a8da8a2945
Huy Lam <commanderh1995@gmail.com>
2021-06-28 11:13:53 -0500

    fixed background image to not repeat and stretch to fit

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -3,7 +3,7 @@ import "./HomePage.css";
 
 const HomePage = () => {
 	return (
-		<div style={ {backgroundImage: `url(${background})`} } className="homepage-container">
+		<div style={ {backgroundImage: `url(${background})`, backgroundRepeat: "no-repeat", backgroundSize: "cover"} } className="homepage-container">
 			<div className="textbox-1">Boba is love, Boba is life</div>
 			<div className="textbox-2">Share your boba, share your favorite places</div>
 		</div>
